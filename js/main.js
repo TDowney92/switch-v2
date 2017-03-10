4713df794777e89ef72497a244c6afcd9d68b982
TDowney92 <tmed425@hotmail.com>
2017-03-10 08:20:19 -0500

    final switch

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@
 // set up DOM
 $(document).ready(function() {
 	$('#switch').on("click", function() {
-		$("html").toggleClass('on');
-		$("h1").toggleClass('on');
-		$("#switch>div").toggleClass('on');
+		$("html, h1, footer, #switch>div").toggleClass('on');
 		console.log('working');
 	});
 });
